10b7af8c16e087a63fad4d9614e8e391ade557d6
hzhfred <z8huang@ucsd.edu>
2020-09-13 03:23:46 -0700

    update

--- public/index.js
+++ public/index.js
@@ -1,43 +1,55 @@
-/*global chrome*/
-function homeClick() {
-  chrome.tabs.executeScript(null, {
-    code: "document.body.innerText"
-  }, receiveText);
+
+
+function content() {
+	// chrome.tabs.executeScript(null, { file: "content.js" });
+
+  /*global chrome*/
+    chrome.tabs.executeScript(null, {
+      code: "document.body.innerText"
+    }, receiveText);
+
+  function receiveText(resultsArray) {
+    let content = resultsArray[0];
+    //"wildfire, black, women"
+
+    // send info to api, get a url back.
+    const api_url = 'https://us-central1-smooth-brains-mask-evaluation.cloudfunctions.net/donationLink';
+    fetch(api_url, {
+      method: 'POST',
+      body: JSON.stringify({text:content}),
+      headers:{
+        'Content-Type': 'application/json'
+      } })
+      .then(data => {
+        alert("Found the Perfect Match !");
+        return data.json();
+      })
+      .then(res => {
+          let urlcontent = res[0].split('//')[1];
+          let arr = urlcontent.split('/');
+          document.getElementById('title').innerText = arr[0].split('.')[1] +"."+ arr[0].split('.')[2];
+          document.getElementById('organization').innerText = arr[arr.length-1];
+
+
+          document.getElementById('clickAnalyze').addEventListener('click', function(){
+            chrome.tabs.update({url: res[0] });
+          });
+
+
+      })
+      .catch(error => console.error('Error:', error));
+
+
+
+  }
+
+
+function newPage(){
 
 }
 
-function receiveText(resultsArray) {
-  alert(resultsArray[0]);
-  let content = resultsArray[0];
-  //"wildfire, black, women"
-
-  // send info to api, get a url back.
-  const api_url = 'https://us-central1-smooth-brains-mask-evaluation.cloudfunctions.net/donationLink';
-  fetch(api_url, {
-    method: 'POST',
-    body: JSON.stringify({text:content}),
-    headers:{
-      'Content-Type': 'application/json'
-    } })
-    .then(data => {
-      alert(data[0]);
-      return data.json();
-    })
-    .then(res => {
-        alert(res[0]);
-      chrome.tabs.update({
-        url: res[0]
-      });
-    })
-    .catch(error => console.error('Error:', error));
 
 
 
-
-  // update
-  // chrome.tabs.update({
-  //   url: "https://mail.google.com/mail/u/0/#inbox"
-  // });
 }
-
-document.getElementById('clickAnalyze').addEventListener('click', homeClick);
+document.getElementById('clickLoad').addEventListener('click', content);
